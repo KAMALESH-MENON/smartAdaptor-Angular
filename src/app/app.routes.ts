a36eee6c7511df1873d6b5ff7fca294306c527f8
kamalesh <kamalcbe86@gmail.com>
2024-01-04 10:11:47 +0530

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterComponent } from './component/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCmXB_TK49mkvk1vkfAdgWwbV_PsoY6aj4',
+    authDomain: 'smart-adaptor.firebaseapp.com',
+    projectId: 'smart-adaptor',
+    storageBucket: 'smart-adaptor.appspot.com',
+    messagingSenderId: '355951971675',
+    appId: '1:355951971675:web:1e972db27198a7779b1e7e',
+  },
+};
